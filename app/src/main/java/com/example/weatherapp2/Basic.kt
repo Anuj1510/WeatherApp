ede360c4660c7ed49db5ce2a45bfe4400f2a6d42
Anuj1510 <anujshahi1510@gmail.com>
2023-03-12 23:21:54 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/weatherapp2/Basic.kt
@@ -0,0 +1,17 @@
+package com.example.weatherapp2
+
+/*
+
+The Retrofit library is a type-safe REST client for Android, Java, and Kotlin, developed by Square.
+ With the help of the Retrofit library, we can have access to a powerful framework that helps us in
+ authenticating and interacting with APIs and sending network requests with OkHttp.
+ With the help of this library, downloading JSON or XML data from a web API becomes easy.
+  In a Retrofit library, once the data is downloaded, it is parsed into a Plain Old Java Object (POJO) which
+  must be defined for each “resource” in the response. Retrofit is an easy and fast library to retrieve and
+  upload data via a REST-based web service.
+
+Retrofit manages the process of receiving, sending, and creating HTTP requests and responses.
+It resolves issues before sending an error and crashing the application. It pools connections to reduce latency.
+ It is used to cache responses to avoid sending duplicate requests.
+
+ */
